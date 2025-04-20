e29a92306943a64c76945ea34b48f6d299aa490a
KEVIN SANTAMARIA <ka3445793@gmail.com>
2025-04-20 15:22:39 -0500

    terminando controllers

---
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/RolController.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.backend.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.backend.dtos.RolDTO;
+import pe.edu.upc.backend.serviceinterfaces.IRolService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/roles")
+public class RolController {
+    @Autowired
+    private IRolService rS;
+
+    @GetMapping
+    private List<RolDTO> listar() {
+
+        return rS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, RolDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
--- Backend/src/main/java/pe/edu/upc/backend/controllers/UsuarioController.java
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/UsuarioController.java
@@ -1,4 +1,47 @@
 package pe.edu.upc.backend.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.backend.dtos.UsuarioDTO;
+import pe.edu.upc.backend.entities.Usuario;
+import pe.edu.upc.backend.serviceinterfaces.IUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService uS;
+
+    @GetMapping
+    public List<UsuarioDTO> listar() {
+        return uS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void insertar(@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario us = m.map(dto, Usuario.class);
+        uS.insert(us);
+    }
+
+    @PostMapping
+    public void modificar(@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario us = m.map(dto, Usuario.class);
+        uS.update(us);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") int id){
+        uS.delete(id);
+    }
 }
