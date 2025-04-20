0a6e80ccf61e80a844ef6ca3d70af1122f8f9cef
KEVIN SANTAMARIA <ka3445793@gmail.com>
2025-04-20 13:13:29 -0500

    agregando atributos de la tabla usuario y rol

--- Backend/src/main/java/pe/edu/upc/backend/entities/Rol.java
+++ Backend/src/main/java/pe/edu/upc/backend/entities/Rol.java
@@ -1,4 +1,40 @@
 package pe.edu.upc.backend.entities;
 
 public class Rol {
+    private int idRol;
+    private String nombreRol;
+    private Usuario user;
+
+    public Rol() {
+    }
+
+    public Rol(int idRol, String nombreRol, Usuario user) {
+        this.idRol = idRol;
+        this.nombreRol = nombreRol;
+        this.user = user;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
 }
--- Backend/src/main/java/pe/edu/upc/backend/entities/Usuario.java
+++ Backend/src/main/java/pe/edu/upc/backend/entities/Usuario.java
@@ -1,4 +1,93 @@
 package pe.edu.upc.backend.entities;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Usuario {
+    private int idUsuario;
+    private String nombreUsuario;
+    private String emailUsuario;
+    private String telefonoUsuario;
+    private String direccionUsuario;
+    private LocalDateTime fechaRegistroUsuario;
+    private String passwordUsuario;
+    private List<Rol>roles;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombreUsuario, String emailUsuario, String telefonoUsuario, String direccionUsuario, LocalDateTime fechaRegistroUsuario, String passwordUsuario, List<Rol> roles) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.emailUsuario = emailUsuario;
+        this.telefonoUsuario = telefonoUsuario;
+        this.direccionUsuario = direccionUsuario;
+        this.fechaRegistroUsuario = fechaRegistroUsuario;
+        this.passwordUsuario = passwordUsuario;
+        this.roles = roles;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getTelefonoUsuario() {
+        return telefonoUsuario;
+    }
+
+    public void setTelefonoUsuario(String telefonoUsuario) {
+        this.telefonoUsuario = telefonoUsuario;
+    }
+
+    public String getDireccionUsuario() {
+        return direccionUsuario;
+    }
+
+    public void setDireccionUsuario(String direccionUsuario) {
+        this.direccionUsuario = direccionUsuario;
+    }
+
+    public LocalDateTime getFechaRegistroUsuario() {
+        return fechaRegistroUsuario;
+    }
+
+    public void setFechaRegistroUsuario(LocalDateTime fechaRegistroUsuario) {
+        this.fechaRegistroUsuario = fechaRegistroUsuario;
+    }
+
+    public String getPasswordUsuario() {
+        return passwordUsuario;
+    }
+
+    public void setPasswordUsuario(String passwordUsuario) {
+        this.passwordUsuario = passwordUsuario;
+    }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
 }
