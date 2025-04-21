af782d17f04e12a9df6bee5238cbd07bbcc02e1d
AndreAlcantar <u202019907@upc.edu.pe>
2025-04-20 23:31:48 -0500

    Agregando entidad Post

--- Backend/src/main/java/pe/edu/upc/backend/serviceimplements/PostImplement.java
+++ Backend/src/main/java/pe/edu/upc/backend/serviceimplements/PostImplement.java
@@ -1,4 +1,33 @@
 package pe.edu.upc.backend.serviceimplements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.backend.entities.Post;
+import pe.edu.upc.backend.repositories.IPostRepository;
+import pe.edu.upc.backend.serviceinterfaces.IPostService;
 
-public class PostImplement {
+import java.util.List;
+@Service
+public class PostImplement implements IPostService {
+    @Autowired
+    private IPostRepository pT;
+
+    @Override
+    public List<Post> list(){
+        return pT.findAll();
+    }
+
+    @Override
+    public void insert(Post pos) {
+        pT.save(pos);
+    }
+
+    @Override
+    public void update(Post pos) {
+        pT.save(pos);
+    }
+
+    @Override
+    public void delete(int id) {
+        pT.deleteById(id);
+    }
 }
