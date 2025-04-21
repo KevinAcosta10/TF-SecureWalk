2d953ab7da45a93f8e59a0a59719f0e383973bae
Jair4lex <jairalexanderalvaloayza@gmail.com>
2025-04-21 11:08:44 -0500

    Agregando  codigo EvaluaciÃon incidente

--- Backend/src/main/java/pe/edu/upc/backend/controllers/EvaluacionIncidenteController.java
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/EvaluacionIncidenteController.java
@@ -1,4 +1,53 @@
 package pe.edu.upc.backend.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.backend.dtos.EncuestaDTO;
+import pe.edu.upc.backend.dtos.EvaluacionIncidenteDTO;
+import pe.edu.upc.backend.entities.Encuesta;
+import pe.edu.upc.backend.entities.EvaluacionIncidente;
+import pe.edu.upc.backend.serviceinterfaces.IEncuestaService;
+import pe.edu.upc.backend.serviceinterfaces.IEvaluacionIncidenteService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/evaluaciones")
 public class EvaluacionIncidenteController {
+    @Autowired
+    private IEvaluacionIncidenteService eIR;
+
+    @GetMapping("/listas")
+    public List<EvaluacionIncidenteDTO> listar(){
+        return eIR.list().stream().map(z->{
+            ModelMapper modelMapper = new ModelMapper();
+            return modelMapper.map(z, EvaluacionIncidenteDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void insertar(@RequestBody EvaluacionIncidenteDTO dto){
+        ModelMapper m = new ModelMapper();
+        EvaluacionIncidente eI = m.map(dto, EvaluacionIncidente.class);
+        eIR.insert(eI);
+    }
+    @GetMapping("/{id}")
+    public EvaluacionIncidenteDTO buscarId(@PathVariable("id") int id){
+        ModelMapper m = new ModelMapper();
+        EvaluacionIncidenteDTO dto =m.map(eIR.listId(id), EvaluacionIncidenteDTO.class);
+        return dto;
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody EvaluacionIncidenteDTO dto){
+        ModelMapper m = new ModelMapper();
+        EvaluacionIncidente eI = m.map(dto, EvaluacionIncidente.class);
+        eIR.update(eI);
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") int id){
+        eIR.delete(id);
+    }
 }
--- Backend/src/main/java/pe/edu/upc/backend/serviceinterfaces/IEncuestaService.java
+++ Backend/src/main/java/pe/edu/upc/backend/serviceinterfaces/IEncuestaService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface IEncuestaService {
     public List<Encuesta> list();
+    public void insert(Encuesta e);
+    public Encuesta listId(int id);
 }
