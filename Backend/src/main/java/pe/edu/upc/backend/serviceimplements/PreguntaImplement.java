c7a3cb38c0f73911a229b11348e45b3cfc7df043
AndreAlcantar <u202019907@upc.edu.pe>
2025-04-20 22:40:51 -0500

    agregando entidad pregunta

---
+++ Backend/src/main/java/pe/edu/upc/backend/serviceimplements/PreguntaImplement.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.backend.serviceimplements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.backend.entities.Pregunta;
+import pe.edu.upc.backend.repositories.IPreguntaRepository;
+import pe.edu.upc.backend.serviceinterfaces.IPreguntaService;
+
+import java.util.List;
+
+@Service
+public class PreguntaImplement implements IPreguntaService {
+    @Autowired
+    private IPreguntaRepository pS;
+
+    @Override
+    public List<Pregunta> list(){
+        return pS.findAll();
+    }
+
+    @Override
+    public void insert(Pregunta preg) {
+        pS.save(preg);
+    }
+
+    @Override
+    public void update(Pregunta preg) {
+        pS.save(preg);
+    }
+
+    @Override
+    public void delete(int id) {
+        pS.deleteById(id);
+    }
+
+}
