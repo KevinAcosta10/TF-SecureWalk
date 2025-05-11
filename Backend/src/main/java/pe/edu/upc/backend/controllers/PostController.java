1db21d93817ffd8197d8def92d407e5292f90e09
Novakls <jheanpier2018@gmail.com>
2025-05-11 03:56:02 -0500

    gaa

--- Backend/src/main/java/pe/edu/upc/backend/controllers/PostController.java
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/PostController.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.backend.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.backend.dtos.PostDTO;
 import pe.edu.upc.backend.dtos.PostPorIncidenteDTO;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/posts")
+@PreAuthorize("hasAnyAuthority('Administrador', 'Usuario')")
 public class PostController {
     @Autowired
     private IPostService pT;
