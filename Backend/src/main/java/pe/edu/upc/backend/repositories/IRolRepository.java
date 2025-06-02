150dfbf4e25d37ac64326286243c61a6210ff6ad
Jair4lex <jairalexanderalvaloayza@gmail.com>
2025-06-02 00:36:13 -0500

    Validacion Roles con Security

--- Backend/src/main/java/pe/edu/upc/backend/repositories/IRolRepository.java
+++ Backend/src/main/java/pe/edu/upc/backend/repositories/IRolRepository.java
@@ -3,7 +3,6 @@ package pe.edu.upc.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
 import pe.edu.upc.backend.entities.Rol;
 
 import java.util.List;
