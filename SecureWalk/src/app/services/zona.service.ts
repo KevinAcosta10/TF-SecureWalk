73eca30304f451ae4d3293b3d48660adca0150e6
KEVIN SANTAMARIA <ka3445793@gmail.com>
2025-06-16 17:27:55 -0500

    implementando insert

--- SecureWalk/src/app/services/zona.service.ts
+++ SecureWalk/src/app/services/zona.service.ts
@@ -4,25 +4,36 @@ import { HttpClient } from '@angular/common/http';
 import { Zona } from '../models/zona';
 import { Subject } from 'rxjs';
 
-const base_url=environment.base
+const base_url = environment.base
 @Injectable({
   providedIn: 'root'
 })
 export class ZonaService {
   private listaCambio = new Subject<Zona[]>()
   private url = `${base_url}/zonas`
-  constructor(private http:HttpClient) { }
-  list(){
+  constructor(private http: HttpClient) { }
+  list() {
     return this.http.get<Zona[]>(`${this.url}/listar`)
   }
   insert(z: Zona) {
-        return this.http.post(`${this.url}/insertar`, z)
-      }
-    
-      setList(listaNueva: Zona[]) {
-        this.listaCambio.next(listaNueva)
-      }
-      getList() {
-        return this.listaCambio.asObservable()
-      }
+    return this.http.post(`${this.url}/insertar`, z)
+  }
+
+  setList(listaNueva: Zona[]) {
+    this.listaCambio.next(listaNueva)
+  }
+  getList() {
+    return this.listaCambio.asObservable()
+  }
+  listId(id: number) {
+    return this.http.get<Zona>(`${this.url}/${id}`)
+  }
+
+  update(z: Zona) {
+    return this.http.put(this.url, z)
+  }
+
+  deleteS(id: number) {
+    return this.http.delete(`${this.url}/${id}`)
+  }
 }
