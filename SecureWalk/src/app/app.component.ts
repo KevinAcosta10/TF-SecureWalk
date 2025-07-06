1516b7a0d123441ffb030f30c2e0ac0bfa0575a7
Novakls <jheanpier2018@gmail.com>
2025-07-06 02:18:37 -0500

    landingpage y layouts

--- SecureWalk/src/app/app.component.ts
+++ SecureWalk/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
