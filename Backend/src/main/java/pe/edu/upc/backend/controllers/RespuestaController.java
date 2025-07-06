761b8987912b1d3367422362da641d912c42ed9f
Jair4lex <jairalexanderalvaloayza@gmail.com>
2025-07-06 18:39:39 -0500

    Cambios menores

--- Backend/src/main/java/pe/edu/upc/backend/controllers/RespuestaController.java
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/RespuestaController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.backend.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.backend.dtos.*;
 import pe.edu.upc.backend.entities.Respuesta;
