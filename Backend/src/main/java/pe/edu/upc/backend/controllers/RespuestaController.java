2ef46c6dd533ba29963004233fd985403ee98658
Novakls <jheanpier2018@gmail.com>
2025-07-06 22:01:09 -0500

    cambios

--- Backend/src/main/java/pe/edu/upc/backend/controllers/RespuestaController.java
+++ Backend/src/main/java/pe/edu/upc/backend/controllers/RespuestaController.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/respuestas")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class RespuestaController {
     @Autowired
     private IRespuestaService rS;
